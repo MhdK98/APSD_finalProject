de3304e7e1feeadf122f2df6b83e179daf7494d1
MhdK98 <88626081+MhdK98@users.noreply.github.com>
2024-04-27 03:11:35 -0500

    Delete PackageTracker directory

--- PackageTracker/src/main/java/miu/edu/cs489/packagetracker/repository/SystemUserDao.java
+++
@@ -1,8 +0,0 @@
-package miu.edu.cs489.packagetracker.repository;
-
-import miu.edu.cs489.packagetracker.entity.PackageSystemUser;
-import org.springframework.data.repository.CrudRepository;
-
-public interface SystemUserDao extends CrudRepository<PackageSystemUser, Integer> {
-
-}
--- PackageTracker/src/main/java/miu/edu/cs489/packagetracker/repository/TicketStatusDao.java
+++
@@ -1,7 +0,0 @@
-package miu.edu.cs489.packagetracker.repository;
-
-import miu.edu.cs489.packagetracker.entity.TicketStatus;
-import org.springframework.data.repository.CrudRepository;
-
-public interface TicketStatusDao extends CrudRepository<TicketStatus, Integer> {
-}
